a60bd9517b4fa299c05b311a992c5d98443afcfc
Amaryllis Pittin <amaryllispittin@gmail.com>
2023-12-14 17:07:18 +0100

    jour 5 - 18: premier essai sur l'étape 1.1, travail sur fetch

--- Portfolio-architecte-sophie-bluel-master/Backend/middlewares/multer-config.js
+++ Portfolio-architecte-sophie-bluel-master/Backend/middlewares/multer-config.js
@@ -22,3 +22,21 @@ const storage = multer.diskStorage({
 })
 
 module.exports = multer({storage}).single('image')
+
+const clientId = '1';
+const projectsURL = "http://localhost:5678/api/users/login";
+
+console.log("avant le fetch");
+fetch(projectsURL)
+.then(response => {
+	if(!response.ok) {
+		throw new Error('Erreur de réseau lors de la récupération des projets');
+	}
+	response.json();
+})
+.then(projects => {
+	console.log('Projets du client:', projects);
+})
+.catch(error => {
+	console.error('Erreur lors de la récupération finale des projets:', error);
+});
