08407f05177ed11021f35f68eb046370e6d3ccb2
Amaryllis Pittin <amaryllispittin@gmail.com>
2023-12-18 16:30:44 +0100

    jour 7 - 18: étape 1.1, tentative de correction du code pour récupérer les données

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/APIrequest.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/APIrequest.js
@@ -1,37 +1,66 @@
-/***function for API request***/
-async function APIrequest() {
+const elementContainer = document.querySelector('.gallery');
+fetch("http://localhost:5678/api-docs/")
+.then((response) => {
+    return response.json();
+})
+.then((imagesTabs) => {
+    console.log(imagesTabs);
+    for(const imageTab of imagesTabs) {
+        const figureElement = document.createElement('figure');
+        figureElement.innerText = imageTab;
+        elementContainer.appendChild(figureElement);
+    }
+});
+
+
+
+/*async function APIrequest() {
     try {
     const projectsURL = "http://localhost:5678/api-docs/#/default/post_users_login";
-    fetch(projectsURL)
+    const response = await fetch(projectsURL);
 
     if (!response.ok) {
         throw new Error('Erreur lors de la récupération des données')
     }
-    const fetchUrl = await fetch(projectsURL)
-    const projectsJson = await fetchUrl.json()
-    console.log(projectsJson)
+
+    const projectsJson = await response.json();
+    console.log(projectsJson);
+
+    dataOnHTML(projectsJson);
+    } catch (error) {
+        console.error('Erreur 2:', error);
+    }
+}*/
+
+
+
+/***Request & Add projects on html***/
+
+/*const elementContainer = document.querySelector('.gallery');
+
+async function APIrequest() {
+    try {
+    const projectsURL = "http://localhost:5678/api-docs/#/default/post_users_login";
+    const response = await fetch(projectsURL);
+
+    if (!response.ok) {
+        throw new Error('Erreur lors de la récupération des données')
+    }
+
+    const projectsJson = await response.json();
+    console.log(projectsJson);
+
     dataOnHTML(projectsJson);
     } catch (error) {
         console.error('Erreur 2:', error);
     }
 }
 
-/***Add projects on html***/
-function dataOnHTML(data) {
-    const projectsContainer = document.querySelector(".gallery");
-
-    data.array.forEach(item => {
-        const projectsElement = document.createElement('figure');
-
-        const imageElement = document.createElement('img');
-        imageElement.src = item.imageUrl;
-        const titleElement = document.createElement('figcaption');
-        titleElement.innerText = item.title;
-
-        projectsElement.appencChild(projectsElement);
-        projectsElement.appendChild(imageElement);
-        projectsContainer.appendChild(projectsElement);
-
-    });
-}
-
+function dataOnHTML(imagesTab) {
+    console.log(imagesTab);
+    for(const imageTab of imagesTab) {
+        const figureElement = document.createElement('figure');
+        figureElement.innerText = imageTab.title;
+        elementContainer.appendChild(figureElement);
+    }
+}*/
