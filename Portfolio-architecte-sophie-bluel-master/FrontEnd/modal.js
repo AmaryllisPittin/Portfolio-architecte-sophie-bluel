27607b78932f015302e2a8286fc71bab3d854d62
Amaryllis Pittin <amaryllispittin@gmail.com>
2024-01-04 16:23:36 +0100

    jour 17 - 18: travil sur étape 3.3

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/modal.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/modal.js
@@ -81,13 +81,15 @@ fetch("http://localhost:5678/api/works")
     });
 });
 
-/***Création de la partie "Ajout photos"***/
+/*************************Création de la partie "Ajout photos"***********************/
+
 let modalContent = document.querySelector('.modal-wrapper');
 const addImagesButton = document.querySelector('.modal-btn-add');
 const modalContainer = document.getElementById('modal')
 let modalContentAdd = document.createElement('div');
 
 modalContentAdd.classList.add('modal-add-wrapper');
+modalContentAdd.id = 'modal-content';
 
 /**header du modalContentAdd**/
 const modalAddTitle = document.createElement('h3');
@@ -105,12 +107,15 @@ modalContentAdd.appendChild(arrowIcon);
 modalContentAdd.appendChild(xIcon);
 
 /**body du modalContentAdd**/
+
 const modalContentAddBody = document.createElement('div');
 modalContentAddBody.classList.add('modal-add-body');
 
 const modalAddInputContainer = document.createElement('div');
 modalAddInputContainer.classList.add('input-container');
+
 /*Ce que contient le modalAddInputContainer*/
+
 const inputContainerFlex = document.createElement('div');
 inputContainerFlex.classList.add('input-container-flex');
 
@@ -119,7 +124,7 @@ imageIcon.classList.add('fa-regular', 'fa-image');
 
 const fileInput = document.createElement('input');
 fileInput.type = 'file';
-fileInput.accept =  'image/*';
+fileInput.accept =  'image/jpeg, image/png';
 fileInput.id = 'file-input';
 
 fileInput.style.display = 'none';
@@ -131,26 +136,48 @@ labelInsertImage.classList.add('insert-image-label');
 
 const pImageFormat = document.createElement('p');
 pImageFormat.innerText = 'jpg, png: 4mo max';
+
 /*Le formulaire*/
+
 const formContainer = document.createElement('div');
 formContainer.classList.add('form-container');
 const addImageForm = document.createElement('form');
 
 const formTitleImage = document.createElement('input');
-fileInput.type = 'text';
-fileInput.id = 'title-input';
+formTitleImage.type = 'text';
+formTitleImage.id = 'title-input';
 const labelTitleImage = document.createElement('label');
 labelTitleImage.htmlFor = 'title-input';
 labelTitleImage.textContent = 'Titre';
 labelTitleImage.classList.add('form-label');
 
-const formCategoryImage = document.createElement('input');
-fileInput.type = 'text';
-fileInput.id = 'category-input';
 const labelCategoryImage = document.createElement('label');
-labelCategoryImage.htmlFor = 'category-input';
+labelCategoryImage.htmlFor = 'category-select';
 labelCategoryImage.textContent = 'Catégorie';
 labelCategoryImage.classList.add('form-label');
+
+const selectCategory = document.createElement('select');
+selectCategory.name = 'categories';
+selectCategory.id = 'category-select';
+
+const optionCategory = document.createElement('option');
+optionCategory.value = '';
+const optionObjectCategory = document.createElement('option');
+optionObjectCategory.value = 'Objets';
+optionObjectCategory.innerText = 'Objets';
+const optionAppCategory = document.createElement('option');
+optionAppCategory.value = 'Appartements';
+optionAppCategory.innerText = 'Appartements';
+const optionHrCategory = document.createElement('option');
+optionHrCategory.value = 'Hôtels & Restaurants';
+optionHrCategory.innerText = 'Hôtels & Restaurants';
+
+
+/*Submit de la modale d'ajout*/
+
+const validButton = document.createElement('button');
+validButton.classList.add('modal-btn-valid');
+validButton.innerText = 'Valider';
 
 fileInput.addEventListener('change', function(event) {     // réagit aux changements, récupération de l'image//
     const file = event.target.files[0];
@@ -168,7 +195,13 @@ inputContainerFlex.appendChild(pImageFormat);
 addImageForm.appendChild(labelTitleImage);
 addImageForm.appendChild(formTitleImage);
 addImageForm.appendChild(labelCategoryImage);
-addImageForm.appendChild(formCategoryImage);
+
+selectCategory.appendChild(optionCategory);
+selectCategory.appendChild(optionObjectCategory);
+selectCategory.appendChild(optionAppCategory);
+selectCategory.appendChild(optionHrCategory);
+addImageForm.appendChild(selectCategory);
+addImageForm.appendChild(validButton);
 
 modalContentAddBody.appendChild(modalAddInputContainer);
 
@@ -180,14 +213,49 @@ modalContentAddBody.appendChild(formContainer);
 
 
 
+modalContentAdd.addEventListener('click', function(e) {
+    stopPropagation(e);
+});
+
+document.getElementById('js-close-modal-icon').addEventListener('click', closeModal);
 
 addImagesButton.addEventListener('click', () => {
-
     modalContent.style.display = 'none';
     modalContainer.appendChild(modalContentAdd);
     modalContent = modalContentAdd;
     modalContentAdd.style.display = 'block';
 })
+
+/*****Fonctions pour valider l'ajout d'images*****/
+
+validButton.addEventListener('click', function (e) {
+    if (formTitleImage.value.length < 1 || selectCategory.value === '') {
+        const errorMessageContainer = document.querySelector('.form-container');
+        const errorMessage = document.createElement('p');
+        errorMessage.innerText = 'Erreur: veuillez remplir les deux champs pour valider'
+        errorMessage.style.display = 'block';
+
+        errorMessageContainer.appendChild(errorMessage);
+        stopPropagation(e);
+
+    }
+});
+
+formTitleImage.addEventListener('input', function () {
+    errorMessage.style.display = 'none'
+});
+
+selectCategory.addEventListener('change', updateValidationFormColor)
+
+function updateValidationFormColor() {
+    if (formTitleImage.value.length >= 1 && selectCategory.value !== '') {
+        validButton.style.backgroundColor = '#1D6154';
+    } else {
+        validButton.style.backgroundColor = '';
+    }
+}
+
+
 
 
 
