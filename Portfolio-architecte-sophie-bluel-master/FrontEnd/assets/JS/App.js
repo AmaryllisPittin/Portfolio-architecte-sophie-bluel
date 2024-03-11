d2b23b6eb1e16e1114ee3df112ac587a9b9da6ca
Amaryllis Pittin <amaryllispittin@gmail.com>
2024-03-11 17:18:39 +0100

    correction de la structure

---
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/JS/App.js
@@ -0,0 +1,124 @@
+let listCategories = [];
+let allProjects = [];
+
+export async function getWorks() {
+  await fetch("http://localhost:5678/api/works")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      allProjects = data;
+    })
+    .then(() => {
+      createGallery();
+    });
+}
+getWorks();
+
+const getCategories = async () => {
+  await fetch("http://localhost:5678/api/categories")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      listCategories = data;
+    })
+    .then(() => {
+      createCategories();
+    });
+};
+getCategories();
+
+const createGallery = (projects) => {
+  const galleryContainer = document.querySelector(".gallery");
+  galleryContainer.innerHTML = "";
+
+  const displayWork = projects || allProjects;
+
+  displayWork.forEach((item) => {
+    const figureElement = document.createElement("figure");
+    const imgElement = document.createElement("img");
+    const titleElement = document.createElement("figcaption");
+
+    imgElement.src = item.imageUrl;
+    titleElement.innerText = item.title;
+
+    figureElement.setAttribute("data-id", item.id);
+
+    figureElement.appendChild(imgElement);
+    figureElement.appendChild(titleElement);
+    galleryContainer.appendChild(figureElement);
+  });
+};
+
+const createCategories = () => {
+  const filterbar = document.querySelector(".buttons-container");
+  const buttonTous = document.createElement("button");
+  buttonTous.setAttribute("id", 0);
+  buttonTous.classList.add("filter-button", "selected");
+  buttonTous.textContent = "Tous";
+  filterbar.appendChild(buttonTous);
+
+  listCategories.forEach((categories) => {
+    const button = document.createElement("button");
+    button.setAttribute("id", categories.id);
+    button.classList.add("filter-button");
+    button.textContent = categories.name;
+    filterbar.appendChild(button);
+
+    console.log('Function createCategories called');
+  });
+  filterWorks();
+};
+
+const filterWorks = () => {
+  document.querySelectorAll(".filter-button").forEach((button) => {
+    button.addEventListener("click", (e) => {
+      const active = document.querySelector(".selected");
+      active.classList.remove("selected");
+      const button = e.target;
+      button.classList.add("selected");
+
+      if (button.id == 0) {
+        createGallery();
+      } else {
+        const filtered = allProjects.filter(
+          (item) => item.categoryId == button.id
+        );
+        createGallery(filtered);
+      }
+    });
+  });
+};
+
+/****************** GESTION DE L'AUTHENTIFICATION ******************/
+export const token = localStorage.getItem("token");
+
+const Auth = () => {
+  const portfolioModifyButton = document.getElementById("portfolio-modified");
+  const buttonsContainer = document.querySelector(".buttons-container");
+
+  if (token) {
+    const loginLinkOnIndex = document.getElementById("login-link");
+    loginLinkOnIndex.textContent = "logout";
+    loginLinkOnIndex.style.cursor = "pointer";
+
+    portfolioModifyButton.style.display = "flex";
+    buttonsContainer.style.display = "flex";
+
+    loginLinkOnIndex.addEventListener("click", function () {
+      const confirmation = confirm(
+        "Êtes-vous sûr de vouloir vous déconnecter ?"
+      );
+      if (confirmation) {
+        localStorage.removeItem("token");
+        window.location.reload();
+      }
+    });
+  } else {
+    portfolioModifyButton.style.display = "none";
+    buttonsContainer.style.display = "flex";
+  }
+};
+
+Auth();
---
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/JS/modal.js
@@ -0,0 +1,353 @@
+import { getWorks } from "./App.js";
+
+const token = localStorage.getItem("token");
+
+/******** GESTION OUVERTURE ET FERMETURE DE LA MODALE ********/
+
+const openModal = function (e) {
+  e.preventDefault();
+
+  if (overlay !== null) {
+    overlay.style.display = "flex";
+    overlay.removeAttribute("aria-hidden");
+    overlay.setAttribute("aria-modal", "true");
+
+    overlay.addEventListener("click", closeModal);
+    document.getElementById("js-close-modal-icon").addEventListener("click", closeModal);
+  } else {
+    console.log("la cible n'a pas été trouvée / ou est null");
+  }
+};
+
+document.getElementById("portfolio-modified").addEventListener("click", openModal);
+
+const modal = document.getElementById("modal-content").addEventListener("click", function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    return;
+  });
+
+const stopPropagation = function (e) {
+  e.stopPropagation();
+};
+
+const closeModal = function () {
+  if (overlay === null) return;
+  if (overlay !== null) {
+    overlay.style.display = "none";
+    overlay.setAttribute("aria-hidden", "true");
+    overlay.removeAttribute("aria-modal");
+  } else {
+    console.log("la cible n'a pas été trouvée / ou est null");
+  }
+};
+
+/******** GESTION DES PROJETS DE LA MODALE ********/
+let modalWorks = [];
+const getWorkModal = async () => {
+  await fetch("http://localhost:5678/api/works")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => (modalWorks = data))
+    .then(() => {
+      createGalleryModal();
+    });
+};
+getWorkModal();
+
+/*** Création de la galerie ***/
+const createGalleryModal = () => {
+  const modalContainer = document.querySelector(".modal-body");
+  modalContainer.innerHTML = "";
+
+  modalWorks.forEach((item) => {
+    const imgElement = document.createElement("img");
+    const figureElement = document.createElement("figure");
+    figureElement.setAttribute("id", item.id);
+
+    imgElement.src = item.imageUrl;
+
+    figureElement.appendChild(imgElement);
+    modalContainer.appendChild(figureElement);
+
+    const spanBinElement = document.createElement("span");
+    const binIcon = document.createElement("i");
+    spanBinElement.classList.add("modal-span-bin");
+    binIcon.classList.add("fa-solid", "fa-trash-can");
+    binIcon.setAttribute("id", item.id);
+
+    binIcon.addEventListener("click", DeleteProject);
+
+    spanBinElement.appendChild(binIcon);
+    figureElement.appendChild(spanBinElement);
+  });
+};
+
+/* GESTION DE SUPPRESSION DE PROJET*/
+const DeleteProject = async (e) => {
+  const id = e.target.id;
+  const confirmation = confirm("Etes-vous sûr de supprimer ce projet ?");
+  if (confirmation) {
+    await fetch(`http://localhost:5678/api/works/${id}`, {
+      method: "DELETE",
+      headers: { Authorization: `Bearer ${token}` },
+    }).then((res) => {
+      if (res.ok) {
+        window.alert("Le projet a été supprimé");
+        getWorkModal();
+        getWorks();
+      } else {
+        console.error("Erreur lors de la suppression du projet:", error);
+      }
+    });
+  }
+};
+
+const AddProject = async (e) => {
+  e.preventDefault();
+  const title = document.getElementById("title-input").value;
+  const category = document.getElementById("category-select").value;
+  const image = document.getElementById("file-input").files[0];
+
+  const formData = new FormData();
+  formData.append("title", title);
+  formData.append("category", category);
+  formData.append("image", image);
+
+  await fetch("http://localhost:5678/api/works", {
+    method: "POST",
+    headers: { Authorization: `Bearer ${token}` },
+    body: formData,
+  }).then((response) => {
+    if (response.ok) {
+      getWorkModal();
+      getWorks();
+      closeModal();
+    }
+  });
+};
+
+let overlay = document.getElementById("modal");
+let arrowIcon;
+let xIcon;
+let modalContentAddBody;
+
+/*CREATION de la partie "AJOUT PHOTOS"*/
+
+const modalAddContainer = () => {};
+
+modalAddContainer();
+
+let modalContent = document.querySelector(".modal-wrapper");
+const addImagesButton = document.querySelector(".modal-btn-add");
+const modalContainer = document.getElementById("modal");
+let modalContentAdd = document.createElement("div");
+
+modalContentAdd.classList.add("modal-add-wrapper");
+modalContentAdd.id = "modal-content";
+
+/*MODALE 2: header du modalContentAdd**/
+
+const modalAddTitle = document.createElement("h3");
+modalAddTitle.innerText = "Ajout photo";
+
+arrowIcon = document.createElement("i");
+arrowIcon.classList.add("fa-solid", "fa-arrow-left");
+
+xIcon = document.createElement("i");
+xIcon.classList.add("fa-solid", "fa-x");
+xIcon.id = "js-close-modal-icon";
+
+modalContentAdd.appendChild(modalAddTitle);
+modalContentAdd.appendChild(arrowIcon);
+modalContentAdd.appendChild(xIcon);
+
+/*AJOUT PHOTOS: body du modalContentAdd*/
+
+modalContentAddBody = document.createElement("div");
+modalContentAddBody.classList.add("modal-add-body");
+
+const modalAddInputContainer = document.createElement("div");
+modalAddInputContainer.classList.add("input-container");
+
+modalContentAddBody.appendChild(modalAddInputContainer);
+modalContentAdd.appendChild(modalContentAddBody);
+
+/*AJOUT PHOTOS: Contenu du modalAddInputContainer pour importer le fichier image*/
+
+const inputContainerFlex = document.createElement("div");
+inputContainerFlex.classList.add("input-container-flex");
+modalAddInputContainer.appendChild(inputContainerFlex);
+
+const imageIcon = document.createElement("i");
+imageIcon.classList.add("fa-regular", "fa-image");
+
+const fileInput = document.createElement("input");
+fileInput.type = "file";
+fileInput.accept = "image/jpeg, image/png";
+fileInput.id = "file-input";
+
+fileInput.style.display = "none";
+
+const labelInsertImage = document.createElement("label");
+labelInsertImage.htmlFor = "file-input";
+labelInsertImage.textContent = "+ Ajouter photo";
+labelInsertImage.classList.add("insert-image-label");
+
+const pImageFormat = document.createElement("p");
+pImageFormat.innerText = "jpg, png: 4mo max";
+
+inputContainerFlex.appendChild(imageIcon);
+inputContainerFlex.appendChild(fileInput);
+inputContainerFlex.appendChild(labelInsertImage);
+inputContainerFlex.appendChild(pImageFormat);
+
+/*AJOUT PHOTOS: Le formulaire*/
+
+const formContainer = document.createElement("div");
+formContainer.classList.add("form-container");
+const addImageForm = document.createElement("form");
+
+const formTitleImage = document.createElement("input");
+formTitleImage.type = "text";
+formTitleImage.id = "title-input";
+const labelTitleImage = document.createElement("label");
+labelTitleImage.htmlFor = "title-input";
+labelTitleImage.textContent = "Titre";
+labelTitleImage.classList.add("form-label");
+
+const labelCategoryImage = document.createElement("label");
+labelCategoryImage.htmlFor = "category-select";
+labelCategoryImage.textContent = "Catégorie";
+labelCategoryImage.classList.add("form-label");
+
+const selectCategory = document.createElement("select");
+selectCategory.name = "categories";
+selectCategory.id = "category-select";
+
+const optionCategory = document.createElement("option");
+optionCategory.value = "";
+const optionObjectCategory = document.createElement("option");
+optionObjectCategory.value = "1";
+optionObjectCategory.innerText = "Objets";
+const optionAppCategory = document.createElement("option");
+optionAppCategory.value = "2";
+optionAppCategory.innerText = "Appartements";
+const optionHrCategory = document.createElement("option");
+optionHrCategory.value = "3";
+optionHrCategory.innerText = "Hôtels & Restaurants";
+
+addImageForm.appendChild(labelTitleImage);
+addImageForm.appendChild(formTitleImage);
+addImageForm.appendChild(labelCategoryImage);
+
+selectCategory.appendChild(optionCategory);
+selectCategory.appendChild(optionObjectCategory);
+selectCategory.appendChild(optionAppCategory);
+selectCategory.appendChild(optionHrCategory);
+addImageForm.appendChild(selectCategory);
+
+formContainer.appendChild(addImageForm);
+modalContentAddBody.appendChild(formContainer);
+
+/*MODALE 2: création du bouton "Valider"*/
+
+const validButton = document.createElement("button");
+validButton.type = "submit";
+validButton.classList.add("modal-btn-valid");
+validButton.innerText = "Valider";
+
+addImageForm.appendChild(validButton);
+
+modalContentAdd.addEventListener("click", function (e) {
+  stopPropagation(e);
+});
+
+modalContentAdd
+  .querySelector("#js-close-modal-icon")
+  .addEventListener("click", closeModal);
+/** */
+
+addImagesButton.addEventListener("click", () => {
+  modalContent.style.display = "none";
+  modalContainer.appendChild(modalContentAdd);
+  modalContentAdd.style.display = "block";
+});
+
+arrowIcon.addEventListener("click", () => {
+  modalContentAdd.style.display = "none";
+  modalContainer.appendChild(modalContent);
+  modalContent.style.display = "block";
+});
+
+/*MODALE 2: PRESENTATION de l'IMAGE dans la modale avant de valider*/
+
+fileInput.addEventListener("change", function (event) {
+  if (event.target.files.length > 0) {
+    const selectedFile = event.target.files[0];
+
+    if (
+      selectedFile.type.startsWith("image/jpeg") ||
+      selectedFile.type.startsWith("image/png")
+    ) {
+      const imageElement = document.createElement("img");
+      imageElement.src = URL.createObjectURL(selectedFile);
+      imageElement.classList.add("selected-image");
+
+      modalAddInputContainer.innerHTML = "";
+      modalAddInputContainer.appendChild(imageElement);
+
+      imageElement.style.width = "30%";
+      imageElement.style.height = "100%";
+    }
+  }
+});
+
+/****TENTATIVE POST******/
+
+fileInput.type = "file";
+fileInput.accept = "image/jpeg, image/png";
+fileInput.id = "file-input";
+fileInput.addEventListener("change", function (event) {
+  updateSelectedFile(event);
+});
+function updateSelectedFile(event) {
+  let selectedFile = event.target.files[0];
+  console.log("Fichier sélectionné :", selectedFile.name);
+}
+modalContentAddBody.appendChild(fileInput);
+modalContentAddBody.addEventListener("submit", AddProject);
+
+/*Comportement de la modale d'ajout*/
+
+let errorMessage;
+
+validButton.addEventListener("click", function () {
+  if (formTitleImage.value.length < 1 || selectCategory.value === "") {
+    const errorMessageContainer = document.querySelector(".form-container");
+    errorMessage = document.createElement("p");
+    errorMessage.innerText =
+      "Erreur: veuillez remplir les deux champs pour valider";
+    errorMessage.style.display = "block";
+
+    errorMessageContainer.appendChild(errorMessage);
+    stopPropagation(e);
+  }
+});
+
+formTitleImage.addEventListener("input", function () {
+  if (errorMessage) {
+    errorMessage.style.display = "none";
+  }
+});
+
+selectCategory.addEventListener("change", updateValidationFormColor);
+
+function updateValidationFormColor() {
+  if (formTitleImage.value.length >= 1 && selectCategory.value !== "") {
+    validButton.style.backgroundColor = "#1D6154";
+  } else {
+    validButton.style.backgroundColor = "";
+  }
+}
